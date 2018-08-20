fd03998d7143d542b4897166a99b4cc51fd2e01c
elan <ivaninhust@126.com>
2018-08-20 23:04:45 +0800

    update

--- src/2018-8-19/demo-01/index.go
+++ src/2018-8-19/demo-01/index.go
@@ -9,10 +9,13 @@ import (
 func main() {
 	var err error
 	var name string
-	name, age := "Elan", 12
+	var age string
+	name, age = "Elan", "12"
 	n, err:= io.WriteString(os.Stdout, "Hello world!\n")
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 	}
 	fmt.Printf("%d byte(s) were written.\n", n)
+	fmt.Printf("name: %v\n", name)
+	fmt.Printf("age: %v\n", age)
 }
